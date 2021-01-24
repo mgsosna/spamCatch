5ecd3ff74e77978fef29ba6af219cc7c18488c1d
mgsosna <matt.g.sosna@gmail.com>
2021-01-24 15:33:20 -0500

    Tidy up JS

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,6 @@ var decisionTemplate = `<strong>Decision:</strong> {0}`;
 function onSubmit(text) {
 
     console.log(text);
-    // updatePrintText(value);
     updateSpamProb(text);
 }
 
@@ -22,24 +21,9 @@ function updateSpamProb(value) {
     d3.json(url).then(prob => {
 
          var color = prob2color((1-prob));
-         // div.html(`Spam probability: <span style="color: ${color}">${100*prob}</span><span style="color:gray">%</span>`);
          div.html(String.format(spamProbTemplate, color, 100*prob));
          updateDecision(prob);
      });
-}
-
-function prob2color(prob) {
-	var r, g, b = 0;
-	if(prob < 0.5) {
-		r = 255;
-		g = Math.round(510 * prob);
-	}
-	else {
-		g = 255;
-		r = Math.round(510 - 510 * prob);
-	}
-	var h = r * 0x10000 + g * 0x100 + b * 0x1;
-	return '#' + ('000000' + h.toString(16)).slice(-6);
 }
 
 function updateDecision(prob) {
@@ -56,6 +40,22 @@ function updateDecision(prob) {
     }
 }
 
+// Modified from https://gist.github.com/mlocati/7210513
+function prob2color(prob) {
+	var r, g, b = 0;
+	if(prob < 0.5) {
+		r = 255;
+		g = Math.round(510 * prob);
+	}
+	else {
+		g = 255;
+		r = Math.round(510 - 510 * prob);
+	}
+	var h = r * 0x10000 + g * 0x100 + b * 0x1;
+	return '#' + ('000000' + h.toString(16)).slice(-6);
+}
+
+// Taken from https://stackoverflow.com/questions/2534803/use-of-string-format-in-javascript
 String.format = function() {
       var s = arguments[0];
       for (var i = 0; i < arguments.length - 1; i++) {
